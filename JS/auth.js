e6a68254365d095f69ff3afc51d65ebb95c553d1
Subash Chhetri <156183094+Subashchhetri946@users.noreply.github.com>
2026-06-14 00:14:51 +0300

    login

--- JS/auth.js
+++ JS/auth.js
@@ -13,4 +13,19 @@ function switchAuthTab(tabName) {
     loginForm.classList.toggle('hidden', tabName !== 'login');
     signupForm.classList.toggle('hidden', tabName !== 'signup');
 }
+const app = loadApp();
+loginForm.addEventListener('submit', event => {
+    event.preventDefault();
 
+    const email = document.getElementById('loginEmail').ariaValueMax.trim();
+    const password = document.getElementById('loginPassword').value;
+    const user = app.users.find(item =>item.email === email && item.password === password);
+
+    if(!user) {
+        alert('Invalid login. Try the demo admin');
+        return;
+    }
+
+    setCurrentUser(user);
+    window.location.href = user.role === 'admin' ? 'admin.html' : 'student.html';
+})
